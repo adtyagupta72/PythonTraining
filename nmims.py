1c10f898ca401b7cbaa6d813e9db82d6c6509489
Aditya Gupta <adtyagupta72@gmail.com>
2025-01-30 19:32:46 +0530

    193230012025 first commit

---
+++ nmims.py
@@ -0,0 +1,28 @@
+print("Hello world")
+
+a = 10
+b = "Aditya"
+c = "Software Developer"
+
+#   a = myFunction()
+#   Use of a => storing age
+#   Use of b => store name
+
+age = 10
+name = "Aditya"
+profession = "Software Developer"
+
+#   age = getUsersAge()
+
+print(age)
+print(type(age))
+
+age = "Four"
+print(age)
+print(type(age))
+
+x = input("Enter first value for sum: ")
+y = input("Enter second value for sum: ")
+z = x + y
+print("Sum: ",z)
+
