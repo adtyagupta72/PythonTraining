5d60e662d72b28b9ef6b34615a8abe2a0fbdc430
Aditya Gupta <adtyagupta72@gmail.com>
2025-05-09 18:41:23 +0530

    Module basic code

--- ModuleSample/main.py
+++ ModuleSample/main.py
@@ -1,3 +1,12 @@
 import mod1
 print("I am from main file!!")
-print(__name__)
+context =__name__
+print(context)
+if context == "__main__":
+    print("Main file was executed!")
+else:
+    print("main.py: I am acting as a module only!!")
+
+print("Counter: ", mod1.counter)
+mod1.counter += 1
+print("Counter: ", mod1.counter)
--- ModuleSample/mod1.py
+++ ModuleSample/mod1.py
@@ -1,2 +1,11 @@
+#import main
+
+counter = 0
+
 print("I am inside Mod1 Module!! Yay!")
-print(__name__)
+context =__name__
+print(context)
+if context == "__main__":
+    print("Mod1 file was executed!")
+else:
+    print("mod1: I am acting as a module only!!")
