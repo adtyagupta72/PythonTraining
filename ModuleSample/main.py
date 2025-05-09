6c739d2b79c5f457fa75c005700a4b64c8d1b900
Aditya Gupta <adtyagupta72@gmail.com>
2025-05-09 18:57:08 +0530

    Implemented module's example code with 1 module.

--- ModuleSample/main.py
+++ ModuleSample/main.py
@@ -1,12 +1,7 @@
 import mod1
-print("I am from main file!!")
-context =__name__
-print(context)
-if context == "__main__":
-    print("Main file was executed!")
-else:
-    print("main.py: I am acting as a module only!!")
 
-print("Counter: ", mod1.counter)
-mod1.counter += 1
-print("Counter: ", mod1.counter)
+my_list = [i + 1 for i in range(5)]
+print("Calling module's function: ", mod1.suml(my_list))
+print("Calling module's function: ", mod1.prodl(my_list))
+print(mod1.suml(my_list) == 15)
+print(mod1.prodl(my_list) == 120)
