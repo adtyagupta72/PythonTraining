f738c44ee25fdd929781f9d54c641e1ab459f07e
Aditya Gupta <adtyagupta72@gmail.com>
2025-05-10 20:17:00 +0530

    fixed importing modules from annother directory

--- ModuleSample/programs/main.py
+++ ModuleSample/programs/main.py
@@ -1,18 +1,19 @@
 #import mod1
 #from mod1 import suml, prodl
 
-import sys, os.path
-# my_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))+'/modules/')
-# sys.path.append(my_dir)
-# import  mo
-sys.path.append('/Users/aditya/Documents/Explore/python/TemproryRepo/ModuleSample/modules')
-##import mo
+import sys
+import os
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from modules import mod1
 
 #from mod1 import suml as sum_list, prodl as multi_list
 
 my_list = [i + 1 for i in range(5)]
-# print("Calling module's function: ", mod1.suml(my_list))
-# print("Calling module's function: ", mod1.prodl(my_list))
+print("Calling module's function: ", mod1.suml(my_list))
+print("Calling module's function: ", mod1.prodl(my_list))
 # print(mod1.suml(my_list) == 15)
 # print(mod1.prodl(my_list) == 120)
 
