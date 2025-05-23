0c141d46477974c475a4198bd2a2b4b2eb69cdff
Aditya Gupta <adtyagupta72@gmail.com>
2025-05-23 20:00:51 +0530

    Functions practice

--- nmims.py
+++ nmims.py
@@ -219,9 +219,42 @@ print(avg)
 # name = input("Enter your name: ")
 # hello(name) # calling the function
 
-def message():
-    print("Enter a value: ")
-print(message())    #
-message = 1
-# int
-print(message)
+# def message():
+#     print("Enter a value: ")
+# print(message())    #None
+# message = 1
+# # int
+# print(message)
+
+def is_year_leap(year):
+    if (year % 4 == 0 and year % 100 != 0):
+        return True
+    if (year % 400 == 0):
+        return True
+    else:
+        return False
+
+def days_in_month(year, month):
+    if month == 2:
+        if is_year_leap(year):
+            return 29
+        else:
+            return 28
+    if month in (4, 6, 9, 11):
+        return 30
+    else:
+        return 31
+
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range (len(test_years) ) :
+    yr = test_years [i]
+    mo = test_months [i]
+    print (yr, mo, "->", end="")
+    result = days_in_month (yr, mo)
+    if result == test_results[i] :
+        print ("OK")
+    else:
+        print ("Failed")
