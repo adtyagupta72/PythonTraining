5175c9a1af9f378100ef0112822549378c2f2e04
Aditya Gupta <adtyagupta72@gmail.com>
2025-05-30 19:12:54 +0530

    added function examples

--- nmims.py
+++ nmims.py
@@ -226,35 +226,78 @@ print(avg)
 # # int
 # print(message)
 
-def is_year_leap(year):
-    if (year % 4 == 0 and year % 100 != 0):
-        return True
-    if (year % 400 == 0):
-        return True
-    else:
-        return False
+# def is_year_leap(year):
+#     if (year % 4 == 0 and year % 100 != 0):
+#         return True
+#     if (year % 400 == 0):
+#         return True
+#     else:
+#         return False
+#
+# def days_in_month(year, month):
+#     if month == 2:
+#         if is_year_leap(year):
+#             return 29
+#         else:
+#             return 28
+#     if month in (4, 6, 9, 11):
+#         return 30
+#     else:
+#         return 31
 
-def days_in_month(year, month):
-    if month == 2:
-        if is_year_leap(year):
-            return 29
-        else:
-            return 28
-    if month in (4, 6, 9, 11):
-        return 30
-    else:
-        return 31
+
+# test_years = [1900, 2000, 2016, 1987]
+# test_months = [2, 2, 1, 11]
+# test_results = [28, 29, 31, 30]
+# for i in range (len(test_years) ) :
+#     yr = test_years [i]
+#     mo = test_months [i]
+#     print (yr, mo, "->", end="")
+#     result = days_in_month (yr, mo)
+#     if result == test_results[i] :
+#         print ("OK")
+#     else:
+#         print ("Failed")
+
+# def scope_test():
+#     x = 148
+#     print(x)
+# scope_test()
+# print(x)
+
+# def scope_check():
+#     print("Checking for scope of var: ", var)
+#
+# var = 1
+# scope_check()
+# print(var)
+
+# var = 2
+#
+# def mult(x):
+#     var = 5
+#     return x * var
+#
+# print(mult(7))
+
+# def scope_check():
+# #    global var
+#     var = 2        #Local
+#     print("Checking for scope of var: ", var)
+#
+# global var
+# #var = 1             #Global
+# scope_check()
+# print(var)
+
+def my_function(my_list_1):
+    print("Print #1:", my_list_1)
+    print("Print #2:", my_list_2)
+    my_list_1 = [0, 1]
+    print("Print #3:", my_list_1)
+    print("Print #4:", my_list_2)
 
 
-test_years = [1900, 2000, 2016, 1987]
-test_months = [2, 2, 1, 11]
-test_results = [28, 29, 31, 30]
-for i in range (len(test_years) ) :
-    yr = test_years [i]
-    mo = test_months [i]
-    print (yr, mo, "->", end="")
-    result = days_in_month (yr, mo)
-    if result == test_results[i] :
-        print ("OK")
-    else:
-        print ("Failed")
+my_list_2 = [2, 3]
+my_function(my_list_2)
+print("Print #5:", my_list_2)
