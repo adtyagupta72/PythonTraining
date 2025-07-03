0f94520762329c5dcd09f2c197c6e351992504a9
Aditya Gupta <adtyagupta72@gmail.com>
2025-07-03 19:13:25 +0530

    module completed, package wip

---
+++ ModuleTestNM/programs/main.py
@@ -0,0 +1,16 @@
+import
+print("===This is Main===")
+print("This is main!")
+print(__name__)
+print("===This is Main===")
+
+#print(module.__counter__)
+# module.__counter__ += 5
+# print(module.__counter__)
+
+#from module import suml, prodl
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(module.suml(zeroes))
+print(module.prodl(ones))
